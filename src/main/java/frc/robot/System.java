7c23d53a0007e3c44331eb92552e3f7605b6344e
Natalie Daleo <csnappyplays@gmail.com>
2025-01-11 16:46:12 -0600

    Initial Project
    all vendors added
    systems framework
    logged robot
    constants
    other stuff eventually

---
+++ src/main/java/frc/robot/System.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class System {
+
+    public System() {
+
+    }
+
+}
---
+++ src/main/java/frc/robot/Util/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot.Util;
+
+public final class Constants {
+ 
+    public static class ControllerConstant {
+        public static final int d_port = 0;
+        public static final int o_port = 1;
+
+        public static final double deadzone = 0.15;
+    }
+
+
+}
