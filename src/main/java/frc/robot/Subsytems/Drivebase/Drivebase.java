2bf190e24beab54db0e04bdb76e55032958a5459
Natalie Daleo <csnappyplays@gmail.com>
2025-02-25 18:17:49 -0600

    odgjd

--- src/main/java/frc/robot/Subsytems/Drivebase/Drivebase.java
+++ src/main/java/frc/robot/Subsytems/Drivebase/Drivebase.java
@@ -52,57 +52,54 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
 
     /** Swerve request to apply during robot-centric path following */
     private final SwerveRequest.ApplyRobotSpeeds m_pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
-    
+
     /* Swerve requests to apply during SysId characterization */
     private final SwerveRequest.SysIdSwerveTranslation m_translationCharacterization = new SwerveRequest.SysIdSwerveTranslation();
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
 
     SwerveModuleState[] states = this.getState().ModuleStates;
-    StructArrayPublisher<SwerveModuleState> publisher = 
-    NetworkTableInstance.getDefault().
-    getStructArrayTopic("Swerve Module States", SwerveModuleState.struct).publish();
+    StructArrayPublisher<SwerveModuleState> publisher = NetworkTableInstance.getDefault()
+            .getStructArrayTopic("Swerve Module States", SwerveModuleState.struct).publish();
 
-
-
-    /* SysId routine for characterizing translation. This is used to find PID gains for the drive motors. */
+    /*
+     * SysId routine for characterizing translation. This is used to find PID gains
+     * for the drive motors.
+     */
     private final SysIdRoutine m_sysIdRoutineTranslation = new SysIdRoutine(
-        new SysIdRoutine.Config(
-            null,        // Use default ramp rate (1 V/s)
-            Volts.of(4), // Reduce dynamic step voltage to 4 V to prevent brownout
-            null,        // Use default timeout (10 s)
-            // Log state with SignalLogger class
-            state -> SignalLogger.writeString("SysIdTranslation_State", state.toString())
-        ),
-        new SysIdRoutine.Mechanism(
-            output -> setControl(m_translationCharacterization.withVolts(output)),
-            null,
-            this
-        )
-    );
+            new SysIdRoutine.Config(
+                    null, // Use default ramp rate (1 V/s)
+                    Volts.of(4), // Reduce dynamic step voltage to 4 V to prevent brownout
+                    null, // Use default timeout (10 s)
+                    // Log state with SignalLogger class
+                    state -> SignalLogger.writeString("SysIdTranslation_State", state.toString())),
+            new SysIdRoutine.Mechanism(
+                    output -> setControl(m_translationCharacterization.withVolts(output)),
+                    null,
+                    this));
 
-    
-
-    /* SysId routine for characterizing steer. This is used to find PID gains for the steer motors. */
+    /*
+     * SysId routine for characterizing steer. This is used to find PID gains for
+     * the steer motors.
+     */
     private final SysIdRoutine m_sysIdRoutineSteer = new SysIdRoutine(
-        new SysIdRoutine.Config(
-            null,        // Use default ramp rate (1 V/s)
-            Volts.of(7), // Use dynamic voltage of 7 V
-            null,        // Use default timeout (10 s)
-            // Log state with SignalLogger class
-            state -> SignalLogger.writeString("SysIdSteer_State", state.toString())
-        ),
-        new SysIdRoutine.Mechanism(
-            volts -> setControl(m_steerCharacterization.withVolts(volts)),
-            null,
-            this
-        )
-    );
+            new SysIdRoutine.Config(
+                    null, // Use default ramp rate (1 V/s)
+                    Volts.of(7), // Use dynamic voltage of 7 V
+                    null, // Use default timeout (10 s)
+                    // Log state with SignalLogger class
+                    state -> SignalLogger.writeString("SysIdSteer_State", state.toString())),
+            new SysIdRoutine.Mechanism(
+                    volts -> setControl(m_steerCharacterization.withVolts(volts)),
+                    null,
+                    this));
 
     /*
      * SysId routine for characterizing rotation.
-     * This is used to find PID gains for the FieldCentricFacingAngle HeadingController.
-     * See the documentation of SwerveRequest.SysIdSwerveRotation for info on importing the log to SysId.
+     * This is used to find PID gains for the FieldCentricFacingAngle
+     * HeadingController.
+     * See the documentation of SwerveRequest.SysIdSwerveRotation for info on
+     * importing the log to SysId.
      */
     private final SysIdRoutine m_sysIdRoutineRotation = new SysIdRoutine(
         new SysIdRoutine.Config(
@@ -132,23 +129,24 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
     /**
      * Constructs a CTRE SwerveDrivetrain using the specified constants.
      * <p>
-     * This constructs the underlying hardware devices, so users should not construct
-     * the devices themselves. If they need the devices, they can access them through
+     * This constructs the underlying hardware devices, so users should not
+     * construct
+     * the devices themselves. If they need the devices, they can access them
+     * through
      * getters in the classes.
      *
-     * @param drivetrainConstants Drivetrain-wide constants for the swerve drive
-     * @param modules             Constants for each specific module
+     * @param drivetrainConstants
+     *            Drivetrain-wide constants for the swerve drive
+     * @param modules
+     *            Constants for each specific module
      */
     public Drivebase(
-        SwerveDrivetrainConstants drivetrainConstants,
-        SwerveModuleConstants<?, ?, ?>... modules
-    ) {
+            SwerveDrivetrainConstants drivetrainConstants,
+            SwerveModuleConstants<?, ?, ?>... modules) {
         super(drivetrainConstants, modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
-
-        
 
         configureAutoBuilder();
     }
@@ -160,30 +158,37 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
     /**
      * Constructs a CTRE SwerveDrivetrain using the specified constants.
      * <p>
-     * This constructs the underlying hardware devices, so users should not construct
-     * the devices themselves. If they need the devices, they can access them through
+     * This constructs the underlying hardware devices, so users should not
+     * construct
+     * the devices themselves. If they need the devices, they can access them
+     * through
      * getters in the classes.
      *
-     * @param drivetrainConstants        Drivetrain-wide constants for the swerve drive
-     * @param odometryUpdateFrequency    The frequency to run the odometry loop. If
-     *                                   unspecified or set to 0 Hz, this is 250 Hz on
-     *                                   CAN FD, and 100 Hz on CAN 2.0.
-     * @param odometryStandardDeviation  The standard deviation for odometry calculation
-     *                                  in the form [x, y, theta]ᵀ, with units in meters
-     *                                  and radians
-     * @param visionStandardDeviation   The standard deviation for vision calculation
-     *                                  in the form [x, y, theta]ᵀ, with units in meters
-     *                                  and radians
-     * @param modules                    Constants for each specific module
+     * @param drivetrainConstants
+     *            Drivetrain-wide constants for the swerve drive
+     * @param odometryUpdateFrequency
+     *            The frequency to run the odometry loop. If
+     *            unspecified or set to 0 Hz, this is 250 Hz on
+     *            CAN FD, and 100 Hz on CAN 2.0.
+     * @param odometryStandardDeviation
+     *            The standard deviation for odometry calculation
+     *            in the form [x, y, theta]ᵀ, with units in meters
+     *            and radians
+     * @param visionStandardDeviation
+     *            The standard deviation for vision calculation
+     *            in the form [x, y, theta]ᵀ, with units in meters
+     *            and radians
+     * @param modules
+     *            Constants for each specific module
      */
     public Drivebase(
-        SwerveDrivetrainConstants drivetrainConstants,
-        double odometryUpdateFrequency,
-        Matrix<N3, N1> odometryStandardDeviation,
-        Matrix<N3, N1> visionStandardDeviation,
-        SwerveModuleConstants<?, ?, ?>... modules
-    ) {
-        super(drivetrainConstants, odometryUpdateFrequency, odometryStandardDeviation, visionStandardDeviation, modules);
+            SwerveDrivetrainConstants drivetrainConstants,
+            double odometryUpdateFrequency,
+            Matrix<N3, N1> odometryStandardDeviation,
+            Matrix<N3, N1> visionStandardDeviation,
+            SwerveModuleConstants<?, ?, ?>... modules) {
+        super(drivetrainConstants, odometryUpdateFrequency, odometryStandardDeviation, visionStandardDeviation,
+                modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
@@ -191,14 +196,16 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
     }
 
     private void configureAutoBuilder() {
-        
+
     }
 
-        /**
-     * The function `getRobotPose` returns the robot's pose after checking and updating it.
+    /**
+     * The function `getRobotPose` returns the robot's pose after checking and
+     * updating it.
      *
-     * @return The `getRobotPose` method is returning the robot's current pose after calling the
-     *     `seedCheckedPose` method with the current pose as an argument.
+     * @return The `getRobotPose` method is returning the robot's current pose after
+     *         calling the
+     *         `seedCheckedPose` method with the current pose as an argument.
      */
     public Pose2d getRobotPose() {
         Pose2d pose = getState().Pose;
@@ -207,12 +214,12 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
 
     // Keep the robot on the field
     private Pose2d keepPoseOnField(Pose2d pose) {
-        
+
         double halfBot = DrivebaseConstants.robotLength.div(2).in(Meters);
         double x = pose.getX();
         double y = pose.getY();
 
-        //WARNING: IF ANTHING BAD IS EVER HAPPENING, IM NOT SURE THIS IS RIGHT
+        // WARNING: IF ANTHING BAD IS EVER HAPPENING, IM NOT SURE THIS IS RIGHT
         double newX = MathUtil.clamp(x, halfBot, (Field.getFieldLength().in(Meters) - halfBot));
         double newY = MathUtil.clamp(y, halfBot, (Field.getFieldLength().in(Meters) - halfBot));
 
@@ -228,44 +235,50 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
     }
 
     /**
-     * Returns a command that applies the specified control request to this swerve drivetrain.
+     * Returns a command that applies the specified control request to this swerve
+     * drivetrain.
      *
-     * @param request Function returning the request to apply
+     * @param request
+     *            Function returning the request to apply
      * @return Command to run
      */
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
     }
 
-    public Command alignWheelsCommand(Rotation2d direction){
-       return run(() -> new SwerveRequest.PointWheelsAt().withModuleDirection(direction));
+    public Command alignWheelsCommand(Rotation2d direction) {
+        return run(() -> new SwerveRequest.PointWheelsAt().withModuleDirection(direction));
     }
 
-    
     /**
      * @param chassisSpeeds
      * @return
-     * If we have issues this is a good place to start. Not confident on the end command
+     *         If we have issues this is a good place to start. Not confident on the
+     *         end command
      */
-    public Command driveRobotCenteric(ChassisSpeeds chassisSpeeds){
-        return new StartEndCommand(() -> new SwerveRequest.ApplyRobotSpeeds().withSpeeds(chassisSpeeds), () -> new SwerveRequest.ApplyRobotSpeeds().withSpeeds(new ChassisSpeeds(0.0, 0.0, 0.0)), this);
+    public Command driveRobotCenteric(ChassisSpeeds chassisSpeeds) {
+        return new StartEndCommand(() -> new SwerveRequest.ApplyRobotSpeeds().withSpeeds(chassisSpeeds),
+                () -> new SwerveRequest.ApplyRobotSpeeds().withSpeeds(new ChassisSpeeds(0.0, 0.0, 0.0)), this);
     }
 
-    public Command faceTargetCommand(Rotation2d faceDirection){
+
+    public Command faceTargetCommand(Rotation2d faceDirection) {
         return run(() -> new SwerveRequest.FieldCentricFacingAngle().withTargetDirection(faceDirection));
     }
 
-    // public Command alignRobotCentericXCommand(double targetDifferece, double  xVelocity){
-        
-    //     return run(() -> new SwerveRequest.ApplyRobotSpeeds()
-    //     .withSpeeds(new ChassisSpeeds(0.0, xVelocity, 0.0))).;
+    // public Command alignRobotCentericXCommand(double targetDifferece, double
+    // xVelocity){
+
+    // return run(() -> new SwerveRequest.ApplyRobotSpeeds()
+    // .withSpeeds(new ChassisSpeeds(0.0, xVelocity, 0.0))).;
     // }
 
     /**
      * Runs the SysId Quasistatic test in the given direction for the routine
      * specified by {@link #m_sysIdRoutineToApply}.
      *
-     * @param direction Direction of the SysId Quasistatic test
+     * @param direction
+     *            Direction of the SysId Quasistatic test
      * @return Command to run
      */
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
@@ -276,7 +289,8 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
      * Runs the SysId Dynamic test in the given direction for the routine
      * specified by {@link #m_sysIdRoutineToApply}.
      *
-     * @param direction Direction of the SysId Dynamic test
+     * @param direction
+     *            Direction of the SysId Dynamic test
      * @return Command to run
      */
     public Command sysIdDynamic(SysIdRoutine.Direction direction) {
@@ -287,10 +301,14 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
     public void periodic() {
         /*
          * Periodically try to apply the operator perspective.
-         * If we haven't applied the operator perspective before, then we should apply it regardless of DS state.
-         * This allows us to correct the perspective in case the robot code restarts mid-match.
-         * Otherwise, only check and apply the operator perspective if the DS is disabled.
-         * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
+         * If we haven't applied the operator perspective before, then we should apply
+         * it regardless of DS state.
+         * This allows us to correct the perspective in case the robot code restarts
+         * mid-match.
+         * Otherwise, only check and apply the operator perspective if the DS is
+         * disabled.
+         * This ensures driving behavior doesn't change until an explicit disable event
+         * occurs during testing.
          */
         SmartDashboard.putNumber("Gyro", this.getPigeon2().getYaw().getValueAsDouble());
         SmartDashboard.putNumber("Drivebase Rotation", this.getRotation3d().getAngle());
@@ -300,10 +318,9 @@ public class Drivebase extends TunerSwerveDrivetrain implements Subsystem {
         if (!m_hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
             DriverStation.getAlliance().ifPresent(allianceColor -> {
                 setOperatorPerspectiveForward(
-                    allianceColor == Alliance.Red
-                        ? kRedAlliancePerspectiveRotation
-                        : kBlueAlliancePerspectiveRotation
-                );
+                        allianceColor == Alliance.Red
+                                ? kRedAlliancePerspectiveRotation
+                                : kBlueAlliancePerspectiveRotation);
                 m_hasAppliedOperatorPerspective = true;
             });
         }
