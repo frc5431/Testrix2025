69507269f5bf2f987d44f06b4610c325d70896d8
AlbedoLinguine <yogevcr@gmail.com>
2025-03-05 15:51:06 -0600

    refactor ElevatorPresetCommand to conditionally execute commands based on ManipJoint mode

--- src/main/java/frc/robot/Commands/Chained/ElevatorPresetCommand.java
+++ src/main/java/frc/robot/Commands/Chained/ElevatorPresetCommand.java
@@ -10,14 +10,14 @@ public class ElevatorPresetCommand extends ParallelCommandGroup {
 
 	public ElevatorPresetCommand(PresetPosition position, Elevator elevator, ManipJoint manipJoint) {
 		addCommands(
-            manipJoint.runManipJointCommand(position.getJointMode()),
-            elevator.runElevatorCommand(position.getElevatorMode())
-				
+			(manipJoint.getMode() != ManipJointPositions.FEED) ? 
+            manipJoint.runManipJointCommand(position.getJointMode()).alongWith(
+            elevator.runElevatorCommand(position.getElevatorMode())) : new ElevatorStowCommand(elevator, manipJoint)
 		);
+		
         // saftey protections
         //If(() -> manipJoint.getMode() != ManipJointPositions.FEED);
 		addRequirements(elevator, manipJoint);
 	}
 
 }
-
